cac408aebb259ab3c8030065497e01283eb01f14
Gyu-hyun Ryu <howlryu@gmail.com>
2013-09-16 20:22:00 +0900

    Flight

--- src/Flight.js
+++ src/Flight.js
@@ -21,20 +21,5 @@ var Flight = cc.Sprite.extend({
 
     onEnterTransitionDidFinish:function(){
 
-
-        // load image file from local file system to CCSpriteFrame, then add into CCAnimation
-        for (int i = 1; i < 15; i++)
-        {
-            char szImageFileName[128] = {0};
-            sprintf(szImageFileName, "Images/grossini_dance_%02d.png", i);
-            animation->addSpriteFrameWithFileName(szImageFileName);
-        }
-
-        animation->setDelayPerUnit(2.8f / 14.0f); // This animation contains 14 frames, will continuous 2.8 seconds.
-        animation->setRestoreOriginalFrame(true); // Return to the 1st frame after the 14th frame is played.
-
-        CCAnimate *action = CCAnimate::create(animation);
-        sprite->runAction(action);  // run action on sprite object
-
     }
 });
